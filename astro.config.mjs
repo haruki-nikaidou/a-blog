0092061d37ae9f3da67d03d1bc1f88e437147989
haruki <meteo.haru@gmail.com>
2023-11-28 19:20:46 +0000

    save

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,18 @@ import { defineConfig, passthroughImageService } from 'astro/config';
 import rehypeKatex from 'rehype-katex';
 import remarkMath from 'remark-math';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-    markdown: {
-        remarkPlugins: [
-            remarkMath,
-        ],
-        rehypePlugins: [
-            [rehypeKatex, {
-            // Katex plugin options
-            }]
-        ]
-    },
-    image: {
-        service: passthroughImageService(),
-    },
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [[rehypeKatex, {
+      // Katex plugin options
+    }]]
+  },
+  image: {
+    service: passthroughImageService()
+  },
+  integrations: [mdx()]
 });
